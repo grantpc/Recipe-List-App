18ab25427faa20189e222db64568a1f26a06d85e
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2022-10-14 13:42:27 -0600

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Grant Christensen on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
